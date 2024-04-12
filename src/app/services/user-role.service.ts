cde66163f912f9c900af9976c88e29248ad016ac
Maria Memollari <mariamemollari01@gmail.com>
2024-04-12 09:54:37 +0200

    Service for user role

--- src/app/services/user-role.service.ts
+++ src/app/services/user-role.service.ts
@@ -1,18 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserRoleService {
-  private role = new BehaviorSubject<any>("");
+  private role = new BehaviorSubject<any>('');
+  private baseUrl: string = 'https://localhost:7006/api/';
 
-  constructor() { }
-  public getRole(){
-    console.log(this.role)
+  constructor(private http: HttpClient) {}
+  public getRole() {
+    console.log(this.role);
     return this.role.asObservable();
   }
-  public setRole(role:any){
-   this.role.next(role);
+  public setRole(role: any) {
+    this.role.next(role);
+  }
+
+  addRoleToUser(request: any) {
+    return this.http.post<any>(`${this.baseUrl}UserRole`, request);
+  }
+  getUserRoles() {
+    return this.http.get<any>(`${this.baseUrl}UserRole/getUserRoles`);
+  }
+  removeRoleFromUser(userId: string, role: number) {
+    return this.http.delete<any>(`${this.baseUrl}UserRole/${userId}/${role}`);
   }
 }
