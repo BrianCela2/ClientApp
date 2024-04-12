d9b66122a75ba8d6b463ba0550b216c4ccf81b1a
Maria Memollari <mariamemollari01@gmail.com>
2024-04-12 10:43:00 +0200

    Small changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { SignupComponent } from './components/forms/signup/signup.component';
 import { DashboardComponent } from './components/tables/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 import { EditUserComponent } from './components/forms/edit-user/edit-user.component';
-import { Routes } from "@angular/router";
 import { CreateRoomComponent } from "./rooms/create-room/create-room.component";
 import { EditRoomComponent } from "./rooms/edit-room/edit-room.component";
 import { RoomDetailsComponent } from "./rooms/room-details/room-details.component";
@@ -27,6 +26,6 @@ export const routes: Routes = [
   {path:'Room/Get/:id',component:RoomDetailsComponent,title:'Room Details'},
   {path:'Room/GetAll',component:RoomListComponent,title:'All Rooms'},
   {path:'NotificationAllUsers',component:SendNotificationsComponent,title:'Notification'},
-  {path:'SearchRooms',component:SearchRoomsComponent,title:'Search'}
+  {path:'SearchRooms',component:SearchRoomsComponent,title:'Search',}
 ];
 
--- src/app/components/tables/user-roles/user-roles.component.ts
+++ src/app/components/tables/user-roles/user-roles.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-roles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-roles.component.html',
   styleUrl: './user-roles.component.css'
 })
