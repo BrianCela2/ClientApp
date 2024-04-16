9830a8ee4bb1994dfd08f70c4d3c4ab92d3b13b0
Maria Memollari <mariamemollari01@gmail.com>
2024-04-16 11:30:17 +0200

    remove create reservation button

--- src/app/components/reservate/reservate.component.ts
+++ src/app/components/reservate/reservate.component.ts
@@ -1,22 +1,38 @@
 // reservate.component.ts
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { StepperComponent } from '../stepper/stepper.component';
-import { StepOneComponent } from '../reservationSteps/step-one/step-one.component';
 import { StepTwoComponent } from '../reservationSteps/step-two/step-two.component';
 import { StepThreeComponent } from '../reservationSteps/step-three/step-three.component';
+import { StepOneComponent } from '../reservationSteps/step-one/step-one.component';
 
 @Component({
   selector: 'app-reservate',
   templateUrl: './reservate.component.html',
   styleUrls: ['./reservate.component.css'],
-  imports:[CommonModule, StepperComponent, StepOneComponent,StepTwoComponent,StepThreeComponent],
-  standalone:true
+  imports: [
+    CommonModule,
+    StepperComponent,
+    StepOneComponent,
+    StepTwoComponent,
+    StepThreeComponent,
+  ],
+  standalone: true,
 })
 export class ReservateComponent {
   stepIndex = 0;
+  isCompleted : boolean = false;
+  @ViewChild(StepOneComponent) stepOneComponent!: StepOneComponent;
+  @ViewChild(StepTwoComponent) stepTwoComponent!: StepTwoComponent;
 
   onStepChanged(index: number) {
     this.stepIndex = index;
+  } 
+  
+  onStepNext() {
+    if (this.stepIndex === 2 && this.stepTwoComponent) {
+      this.stepTwoComponent.createReservation();
+    }
   }
+  
 }
--- src/app/components/stepper/stepper.component.ts
+++ src/app/components/stepper/stepper.component.ts
@@ -11,11 +11,13 @@ export class StepperComponent {
   @Input() steps: string[] = [];
   @Output() stepChanged = new EventEmitter<number>();
   currentStepIndex = 0;
+  @Output() nextButtonClicked = new EventEmitter();
 
   nextStep() {
     if (this.currentStepIndex < this.steps.length - 1) {
       this.currentStepIndex++;
       this.stepChanged.emit(this.currentStepIndex);
+      this.nextButtonClicked.emit();
     }
   }
 
