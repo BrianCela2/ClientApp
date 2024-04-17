ab6e82cc50abc9a3d7523e1b22578071b8b343b2
Maria Memollari <mariamemollari01@gmail.com>
2024-04-17 11:23:13 +0200

    change reservation status

--- src/app/components/forms/edit-reservation/edit-reservation.component.ts
+++ src/app/components/forms/edit-reservation/edit-reservation.component.ts
@@ -7,47 +7,71 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-edit-reservation',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-reservation.component.html',
-  styleUrl: './edit-reservation.component.css'
+  styleUrl: './edit-reservation.component.css',
 })
 export class EditReservationComponent {
-reservationId!:string;
-reservationDTO:any={};
-reservationRooms:any;
+  reservationId!: string;
+  reservationDTO: any = {};
+  reservationRooms: any;
 
-constructor(private router:Router,private route: ActivatedRoute, private reservationService: ReservationService) {}
-ngOnInit(): void {
-  this.reservationId = this.route.snapshot.paramMap.get('id') || '';
-  this.reservationDTO = {
-    reservationId: this.reservationId,
-    reservationRooms: [] 
-  };
-  this.reservationService.getReservationById(this.reservationId).subscribe({
-    next: (response: any) => {
-      this.reservationDTO.reservationId = response.id;
-      this.reservationDTO.reservationRooms = response.reservationRooms;
-      // this.reservationDTO.reservationRooms.forEach((reservationRooms:any) => {
-      //   reservationRooms.checkInDate = new Date(reservationRooms.checkInDate);
-      //   reservationRooms.checkOutDate = new Date(reservationRooms.checkOutDate);
-      // });
-      console.log(response);
-    },
-    error: (error) => {
-      console.error('Error fetching room data:', error);
-    }
-  });
-}
-updateReservation(): void {
-  this.reservationService.updateReservation(this.reservationId, this.reservationDTO).subscribe({
-    next: (response) => {
-      console.log('Room updated successfully',response);
-    },
-    error: (error) => {
-      console.error('Error updating room:', error);
-    },
-    complete: () => {
-    }
-  });
-}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private reservationService: ReservationService
+  ) {}
+  ngOnInit(): void {
+    this.reservationId = this.route.snapshot.paramMap.get('id') || '';
+    this.reservationDTO = {
+      reservationId: this.reservationId,
+      reservationRooms: [],
+    };
+    this.reservationService.getReservationById(this.reservationId).subscribe({
+      next: (response: any) => {
+        this.reservationDTO.reservationId = response.id;
+        this.reservationDTO.reservationRooms = response.reservationRooms;
+        this.reservationDTO.reservationRooms.forEach(
+          (reservationRooms: any) => {
+            reservationRooms.checkInDate = new Date(
+              reservationRooms.checkInDate
+            );
+            reservationRooms.checkOutDate = new Date(
+              reservationRooms.checkOutDate
+            );
+          }
+        );
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Error fetching room data:', error);
+      },
+    });
+  }
+  updateReservation(): void {
+    this.reservationService
+      .updateReservation(this.reservationId, this.reservationDTO)
+      .subscribe({
+        next: (response) => {
+          console.log('Room updated successfully', response);
+        },
+        error: (error) => {
+          console.error('Error updating room:', error);
+        },
+        complete: () => {},
+      });
+  }
+
+  cancelReservation(){
+    this.reservationService
+    .updateReservationStatus(this.reservationId)
+      .subscribe({
+        next: (response) => {
+          console.log('Reservation status updated successfully', response);
+        },
+        error: (error) => {
+          console.error('Error updating status:', error);
+        },
+      });
+  }
 }
