66a5891035d140a1d502e7ec8c51bf865234eccf
Maria Memollari <mariamemollari01@gmail.com>
2024-04-26 00:02:31 +0200

    Added some opacity when select a room

--- src/app/components/reservationSteps/step-two/step-two.component.ts
+++ src/app/components/reservationSteps/step-two/step-two.component.ts
@@ -24,34 +24,45 @@ export class StepTwoComponent implements OnInit {
 
   ngOnInit(): void {
     this.roomLists = [];
-    
-      this.http
-        .post<any>('https://localhost:7006/Room/SearchRooms', this.searchParameters)
-        .subscribe({
-          next: (response) => {
-            this.roomLists = response;
-            console.log('roomList', this.roomLists);
-          },
-          error: (error) => {
-            console.error('Error searching rooms:', error);
-          },
-        });
+
+    this.http
+      .post<any>(
+        'https://localhost:7006/Room/SearchRooms',
+        this.searchParameters
+      )
+      .subscribe({
+        next: (response) => {
+          this.roomLists = response;
+          console.log('roomList', this.roomLists);
+        },
+        error: (error) => {
+          console.error('Error searching rooms:', error);
+        },
+      });
   }
 
-  selectRoom(roomId: string, checkInDate: Date, checkOutDate: Date, index: number) {
+  selectRoom(
+    roomId: string,
+    checkInDate: Date,
+    checkOutDate: Date,
+    index: number
+  ) {
     this.selectedRooms[index] = { roomId, checkInDate, checkOutDate };
     this.onStepCompleted.emit();
   }
-  
+  isRoomSelected(roomId: string): boolean {
+    return this.selectedRooms.some((room) => room.roomId === roomId);
+  }
+
   createReservation() {
-    console.log(this.selectedRooms)
+    console.log(this.selectedRooms);
     const reservationRooms = this.selectedRooms.map((room) => ({
       roomId: room.roomId,
       checkInDate: room.checkInDate,
       checkOutDate: room.checkOutDate,
     }));
 
-    this.searchService.setReservation ({
+    this.searchService.setReservation({
       ReservationDate: new Date(),
       ReservationStatus: ReservationStatusEnum.Confirmed,
       ReservationRooms: reservationRooms,
