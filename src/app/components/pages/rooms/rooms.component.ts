ce2ea510d49e1e76ff879845df96f587c0b003be
Maria Memollari <mariamemollari01@gmail.com>
2024-04-25 14:04:41 +0200

    Removed pagination

--- src/app/components/pages/rooms/rooms.component.ts
+++ src/app/components/pages/rooms/rooms.component.ts
@@ -11,26 +11,18 @@ import { PaginationComponent } from '../../tablePagination/pagination/pagination
   styleUrl: './rooms.component.css'
 })
 export class RoomsComponent implements OnInit {
-  public currentPage: number = 1;
-  public pageSize: number = 10;
   rooms: any[] = [];
-  public totalPages!:number ;
 
  constructor(private roomService: RoomService){}
   ngOnInit(): void {
     this.fetchRooms()
   }
   fetchRooms() {
-   this.roomService.getRoomsByCategory(this.currentPage, this.pageSize).subscribe(data => {
-     this.rooms = data.rooms.map((room: any) => ({ ...room, activePhotoIndex: 0 }));
-     this.totalPages =data.totalPages;
+   this.roomService.getRoomsByCategory().subscribe(data => {
+     this.rooms = data?.map((room: any) => ({ ...room, activePhotoIndex: 0 }));
    });
  }
   setActivePhoto(room: any, photoIndex: number) {
     room.activePhotoIndex = photoIndex;
-  }
-  onPageChange(page: number) {
-    this.currentPage = page;
-    this.fetchRooms();
   }
 }
--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -9,8 +9,8 @@ export class RoomService {
   private url = 'https://localhost:7006/Room'; 
   constructor(private httpClient: HttpClient) { }
 
-  getRoomsByCategory(page: number, pageSize: number): Observable<any> {
-    return this.httpClient.get<any>(`${this.url}/GetAll?page=${page}&pageSize=${pageSize}`);
+  getRoomsByCategory(): Observable<any> {
+    return this.httpClient.get<any>(`${this.url}/GetAll`);
   }
   getRooms(page: number, pageSize: number, sortField: string, sortOrder: string): Observable<any> {
     return this.httpClient.get<any>(`${this.url}/GetRoomPhotos?page=${page}&pageSize=${pageSize}&sortField=${sortField}&sortOrder=${sortOrder}`);
