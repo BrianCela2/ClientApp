876b1835c021d1be44a12795ed6f4a917ca1d0cf
Maria Memollari <mariamemollari01@gmail.com>
2024-04-25 22:00:15 +0200

    rooms at homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SearchService } from '../../services/search-service.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RoomService } from '../../services/room.service';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterModule,CommonModule,FormsModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
 })
-export class HomepageComponent {
-  constructor(private router:Router, private searchService: SearchService){}
+export class HomepageComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private searchService: SearchService,
+    private roomService: RoomService
+  ) {}
   checkInDate!: string;
   checkOutDate!: string;
   capacity!: number;
   rooms!: number;
+  roomsService: any[] = [];
 
+  ngOnInit(): void {
+    this.fetchRooms();
+  }
+  setActivePhoto(room: any, photoIndex: number) {
+    room.activePhotoIndex = photoIndex;
+  }
   navigateTo(route: string) {
     this.router.navigate([route]);
   }
@@ -28,12 +40,20 @@ export class HomepageComponent {
       searchParameters.push({
         checkInDate: this.checkInDate,
         checkOutDate: this.checkOutDate,
-        capacity: this.capacity
+        capacity: this.capacity,
       });
     }
 
     this.searchService.setSearchParameters(searchParameters);
 
     this.router.navigateByUrl('/createReservation');
+  }
+  fetchRooms() {
+    this.roomService.getRoomsByCategory().subscribe((data) => {
+      this.roomsService = data?.map((room: any) => ({
+        ...room,
+        activePhotoIndex: 0,
+      }));
+    });
   }
 }
